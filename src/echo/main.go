540048d0ac86a754fa1eef5ab2f4ec20ffd078cb
Alex Mirtoff <alex@mirtoff.ru>
2017-12-20 17:56:33 +0300

    .

--- src/echo/main.go
+++ src/echo/main.go
@@ -2,23 +2,26 @@ package main
 
 import (
 	"net/http"
-	
 	"github.com/labstack/echo"
 )
 
 func main() {
+	// nameVar := "user"
+	// passVar := "passs"
+	//dataVar := "test"
+
 	e := echo.New()
-	e.GET("/", getRoot)
-	e.GET("/user/:id", getUser)
+
+	//e.GET("/", getRoot)
+	e.GET("/get", func(c echo.Context, dataVar string) error{
+		return dataVar
+	})
+
+
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-func getRoot(c echo.Context) error {
+func getRoot(c echo.Context, s string) error {
 	return c.String(http.StatusOK, "Hello, World!")
-}
-
-func getUser(c echo.Context) error {
-	id := c.Param("id")
-	return c.String(http.StatusOK, id)
 }
